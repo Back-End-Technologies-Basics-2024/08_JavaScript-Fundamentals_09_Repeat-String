1be27c01fb1a9693c5dc4d3aa711652ff83847d6
DesiZaharieva <135128496+DesiZaharieva@users.noreply.github.com>
2024-02-06 01:12:31 +0200

    Add files via upload

---
+++ 09_Repeat String.js
@@ -0,0 +1,24 @@
+function solve(inputString, n) 
+{ 
+    'use strict';
+    let stringResult = '';
+
+    for(let i = 0;i < n; i++) {
+        stringResult += inputString;
+    }
+
+
+
+
+    return stringResult;
+
+ 
+    let result = repeatString(originalString, repeatCount);
+    console.log(result);
+
+
+}
+
+let result = solve("abc", 3);
+console.log(result);
+
